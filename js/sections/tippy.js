371552bcd18812db8f3c06c45b7af4beab4b4bd1
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2021-08-22 11:46:34 +0200

    tippy

---
+++ js/sections/tippy.js
@@ -0,0 +1,10 @@
+tippy(".tiptool", {
+  animateFill: true,
+  animation: "scale",
+  /* duration: 500, */
+  followCursor: true,
+  maxWidth: 150,
+  offset: [0, 10],
+  placement: "top",
+  arrow: true,
+});
