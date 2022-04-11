ef021346cd2fe40cb1b2ee22c5c652b4acde3bec
Henrique Vieira <henrique@mvstudio.be>
2022-04-11 08:51:05 +0200

    wip

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-# JS-Libraries
+# JS LIBRARIES
 
-https://vieirahenrique.github.io/JS-Libraries/
+## Swiper
+
+CDN JS
+https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.0.9/swiper-bundle.min.js
+
+CDN CSS
+https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.0.9/swiper-bundle.css
+
+Markup structure
+
+Structure:
+
+        .swiper-container
+            .swiper-wrapper
+                .swiper-slide
+                .swiper-slide
+                .swiper-slide
+
+        If we need, we can use :
+            .swiper-pagination
+            .swiper-button-prev
+            .swiper-button-next
+            .swiper-scrollbar
+        These don't need to be in the swiper container. Only make sure you call them in the JS ny their name.
+
+JS:
+
+```js
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  grabCursor: true,
+
+  on: {
+    slideChange: function (swiper) {
+      from.innerText = `0${this.realIndex + 1}`;
+      of.innerText = `0${swiper.slides.length - 2}`;
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+```
+
+
+
--- js/sections/isotope.js
+++ js/sections/isotope.js
@@ -2,51 +2,42 @@
 
 // init Isotope
 var iso = new Isotope(".grid", {
-  itemSelector: ".element-item",
-  layoutMode: "fitRows",
+    itemSelector: ".element-item",
+    masonry: {
+        gutter: 50,
+    },
 });
 
 // filter functions
-var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function (itemElem) {
-    var number = itemElem.querySelector(".number").textContent;
-    return parseInt(number, 10) > 50;
-  },
-  // show if name ends with -ium
-  ium: function (itemElem) {
-    var name = itemElem.querySelector(".name").textContent;
-    return name.match(/ium$/);
-  },
-};
+var filterFns = {};
 
 // bind filter button click
 var filtersElem = document.querySelector(".filters-button-group");
 filtersElem.addEventListener("click", function (event) {
-  // only work with buttons
-  if (!matchesSelector(event.target, "button")) {
-    return;
-  }
-  var filterValue = event.target.getAttribute("data-filter");
-  // use matching filter function
-  filterValue = filterFns[filterValue] || filterValue;
-  iso.arrange({ filter: filterValue });
+    // only work with buttons
+    if (!matchesSelector(event.target, "button")) {
+        return;
+    }
+    var filterValue = event.target.getAttribute("data-filter");
+    // use matching filter function
+    filterValue = filterFns[filterValue] || filterValue;
+    iso.arrange({ filter: filterValue });
 });
 
 // change is-checked class on buttons
 var buttonGroups = document.querySelectorAll(".button-group");
 for (var i = 0, len = buttonGroups.length; i < len; i++) {
-  var buttonGroup = buttonGroups[i];
-  radioButtonGroup(buttonGroup);
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup(buttonGroup);
 }
 
 function radioButtonGroup(buttonGroup) {
-  buttonGroup.addEventListener("click", function (event) {
-    // only work with buttons
-    if (!matchesSelector(event.target, "button")) {
-      return;
-    }
-    buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
-    event.target.classList.add("is-checked");
-  });
+    buttonGroup.addEventListener("click", function (event) {
+        // only work with buttons
+        if (!matchesSelector(event.target, "button")) {
+            return;
+        }
+        buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+        event.target.classList.add("is-checked");
+    });
 }
